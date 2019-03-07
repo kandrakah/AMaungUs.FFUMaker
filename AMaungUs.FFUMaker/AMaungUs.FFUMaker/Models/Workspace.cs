e6b4f2b51ce77ea0d261d10473b617978a185cc6
Min Maung <min@maungs.com>
2019-03-06 19:06:39 -0600

    Adding Viewmodel for workspace

---
+++ AMaungUs.FFUMaker/AMaungUs.FFUMaker/Models/Workspace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMaungUs.FFUMaker.Models
+{
+    public class Workspace
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string OEMName { get; set; }
+        public string Architecture { get; set; }
+    }
+}
---
+++ AMaungUs.FFUMaker/AMaungUs.FFUMaker/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,49 @@
+﻿using AMaungUs.FFUMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AMaungUs.FFUMaker.ViewModels
+{
+    public class WorkspaceViewModel : BaseViewModel
+    {
+        ObservableCollection<Workspace> workspace;
+        public ObservableCollection<Workspace> WorkSpaces
+        {
+            get
+            {
+                return workspace == null ? new Workspace() : workspace;
+            }
+            set {
+                SetProperty(ref workspace, value);
+            }
+        }
+        public WorkspaceViewModel()
+        {
+            AddWorkSpaceCommand = new DelegateCommand<object>(this.AddWorkspaceCommandExec, x => true);
+            OnPropertyChanged("AddWorkSpaceCommand");
+            DelWorkSpaceCommand = new DelegateCommand<object>(this.DelWorkspaceCommandExec, x => true);
+            OnPropertyChanged("DelWorkSpaceCommand");
+        }
+        System.Windows.Input.ICommand addWorkSpaceCommand;
+        public ICommand AddWorkSpaceCommand
+        {
+            get; set;
+        }
+        private void AddWorkspaceCommandExec(object parm)
+        {
+        }
+        System.Windows.Input.ICommand delWorkSpaceCommand;
+        public ICommand DelWorkSpaceCommand
+        {
+            get; set;
+        }
+        private void DelWorkspaceCommandExec(object parm)
+        {
+        }
+    }
+}
