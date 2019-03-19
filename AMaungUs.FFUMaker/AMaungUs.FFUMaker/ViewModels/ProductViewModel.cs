a496e17762bb010a6703e29ca3a675035a2de362
Min Maung <min@maungs.com>
2019-03-19 12:09:50 -0500

    Adding hook for Creating new products

--- AMaungUs.FFUMaker/AMaungUs.FFUMaker/ViewModels/ProductViewModel.cs
+++ AMaungUs.FFUMaker/AMaungUs.FFUMaker/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using AMaungUs.FFUMaker.Models;
+using AMaungUs.FFUMaker.Views;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AMaungUs.FFUMaker.ViewModels
 {
@@ -37,6 +39,42 @@ namespace AMaungUs.FFUMaker.ViewModels
             var savedjson = Properties.Settings.Default.Workspaces;
             var _workspaces = JsonConvert.DeserializeObject<ObservableCollection<Workspace>>(savedjson);
             workspaces = _workspaces;
+            LoadProducts();
+        }
+        private void LoadProducts()
+        {
+
+        }
+        System.Windows.Input.ICommand createproductcommand;
+        public ICommand CreateProductCommand
+        {
+            get { return createproductcommand == null ? new DelegateCommand<object>(this.CreateProductCmdExec, x => true) : createproductcommand; }
+            set { SetProperty(ref createproductcommand, value); }
+        }
+        private void CreateProductCmdExec(object parm)
+        {
+            CreateProductModal createModal = new CreateProductModal();
+            Nullable<bool> dialogresult = createModal.ShowDialog();
+            if (dialogresult.Value == true)
+            {
+                //var workspace = ((CreateWorkspaceViewModel)createModal.DataContext).workspace;
+                //Workspaces.Add(workspace);
+                //SaveWorkspaces();
+            }
+        }
+        System.Windows.Input.ICommand deleteproductcommand;
+        public ICommand DeleteProductCommand
+        {
+            get { return deleteproductcommand == null ? new DelegateCommand<object>(this.DelProductCommandExec, x => true) : deleteproductcommand; }
+            set { SetProperty(ref deleteproductcommand, value); }
+        }
+        private void DelProductCommandExec(object parm)
+        {
+            if (parm is Workspace)
+            {
+                //var deleteObject = (Workspace)parm;
+                //DeleteWorkspace(deleteObject);
+            }
         }
     }
 }
